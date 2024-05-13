bb91ffe21a657e4eccfae249aa629b06301dc6a6
Gaabriel Ponomarjov <gaabriel.zachar@ivkhk.ee>
2024-05-13 09:55:29 +0300

    venv

--- main.py
+++ main.py
@@ -2,4 +2,12 @@
 
 # Imports
 from customtkinter import *
-# made venv
+
+app = CTk()
+app.title("Weather APP")
+app.geometry("500x400")
+
+label = CTkLabel(master=app)
+label.place(relx=0.5, rely=0.5, anchor='center')
+
+app.mainloop()
