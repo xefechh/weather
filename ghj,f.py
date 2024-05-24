6f0f7a9682421e338d832b55b2e708e719c69c76
artem970 <157696613+artem970@users.noreply.github.com>
2024-05-24 13:14:25 +0300

    отрисовка основных фигур

---
+++ ghj,f.py
@@ -0,0 +1,44 @@
+# Authors: Artem Kii, Gaabriel Ponomarjov
+
+# Imports
+import customtkinter
+
+class SquareApp(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+    
+    def von (self):
+
+        self.title("Weather APP")
+
+        # Создаем холст с заданными размерами
+        self.canvas = customtkinter.CTkCanvas(self, width=1000, height=600)
+        self.canvas.pack()
+
+        customtkinter.set_appearance_mode("Dark")
+    def fig(self, x1, y1, x2, y2):
+
+        # Указываем координаты для квадрата
+        self.x1, self.y1 = x1, y1  # Верхний левый угол
+        self.x2, self.y2 = x2, y2  # Нижний правый угол
+
+        self.draw_square(x1, y1, x2, y2)
+
+    def draw_square(self, x1, y1, x2, y2):
+        self.canvas.create_rectangle(x1, y1, x2, y2, fill="#333333")
+
+app = SquareApp()
+app.von()
+app.fig(30,60,240,250)
+app.fig(30,306,240,576)
+app.fig(270,60,970,440)
+app.fig(270,476,348.75,576)
+app.fig(358.75,476,437.5,576)
+app.fig(447.5,476,526.25,576)
+app.fig(536.25,476,615,576)
+app.fig(625,476,703.75,576)
+app.fig(713.75,476,792.5,576)
+app.fig(802.5,476,881.25,576)
+app.fig(891.25,476,970,576)
+app.mainloop()
+
