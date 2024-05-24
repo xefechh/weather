e449bb1b4d6ab0a11abf8c9163d6973e015388d0
artem970 <157696613+artem970@users.noreply.github.com>
2024-05-24 13:40:37 +0300

    Update ghj,f.py

--- ghj,f.py
+++ ghj,f.py
@@ -1,23 +1,20 @@
-# Authors: Artem Kii, Gaabriel Ponomarjov
+from customtkinter import *
 
-# Imports
-import customtkinter
-
-class SquareApp(customtkinter.CTk):
+class SquareApp(CTk):
     def __init__(self):
         super().__init__()
+        self.von()  # Вызов метода для инициализации окна
     
-    def von (self):
-
+    def von(self):
         self.title("Weather APP")
 
         # Создаем холст с заданными размерами
-        self.canvas = customtkinter.CTkCanvas(self, width=1000, height=600)
+        self.canvas = CTkCanvas(self, width=1000, height=600)
         self.canvas.pack()
 
-        customtkinter.set_appearance_mode("Dark")
-    def fig(self, x1, y1, x2, y2):
+        set_appearance_mode("Dark")
 
+    def fig(self, x1, y1, x2, y2):
         # Указываем координаты для квадрата
         self.x1, self.y1 = x1, y1  # Верхний левый угол
         self.x2, self.y2 = x2, y2  # Нижний правый угол
@@ -28,7 +25,6 @@ class SquareApp(customtkinter.CTk):
         self.canvas.create_rectangle(x1, y1, x2, y2, corner_radius=10, fill="#333333")
 
 app = SquareApp()
-app.von()
 app.fig(30,60,240,250)
 app.fig(30,306,240,576)
 app.fig(270,60,970,440)
@@ -41,4 +37,8 @@ app.fig(713.75,476,792.5,576)
 app.fig(802.5,476,881.25,576)
 app.fig(891.25,476,970,576)
 app.mainloop()
+
+
+
+
 
