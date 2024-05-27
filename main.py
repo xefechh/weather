9c625be38c2b367db6962e198f07381893787ed3
artem970 <157696613+artem970@users.noreply.github.com>
2024-05-27 11:00:02 +0300

    темпиратура на сегодня

--- main.py
+++ main.py
@@ -3,11 +3,43 @@
 # Imports
 from customtkinter import *
 
-app = CTk()
-app.title("Weather APP")
-app.geometry("500x400")
+class WeatherApp(CTk):
+    def __init__(self):
+        super().__init__()
 
-label = CTkLabel(master=app)
-label.place(relx=0.5, rely=0.5, anchor='center')
+    def setup_ui(self, a):
+        self.title("Weather APP")
 
-app.mainloop()
+        set_appearance_mode("Dark")
+
+        # Создаем холст с заданными размерами
+        self.canvas = CTkCanvas(self, width=1000, height=600 , bg = "#000000", highlightthickness=0)
+        self.canvas.pack()
+
+        # Отрисовка всех квадратов
+        self.draw_square(30, 60, 240, 250, text=a)
+        self.draw_square(30, 306, 240, 576, text="5 days forecast")
+        self.draw_square(270, 60, 970, 440)
+        
+        # Отрисовка временных слотов
+        time_slots = [
+            (270, 476, 348.75, 576),
+            (358.75, 476, 437.5, 576),
+            (447.5, 476, 526.25, 576),
+            (536.25, 476, 615, 576),
+            (625, 476, 703.75, 576),
+            (713.75, 476, 792.5, 576),
+            (802.5, 476, 881.25, 576),
+            (891.25, 476, 970, 576),
+        ]
+        for x1, y1, x2, y2 in time_slots:
+            self.draw_square(x1, y1, x2, y2)
+
+    def draw_square(self, x1, y1, x2, y2, text=""):
+        self.canvas.create_rectangle(x1, y1, x2, y2, fill="#333333")
+        if text:
+            # Добавляем текст в центр квадрата
+            x_center = (x1 + x2) / 2
+            y_center = (y1 + y2) / 2
+            self.canvas.create_text(x_center, y_center, text=text, fill="white", font=("Arial", 16, "bold"))
+
--- try1.py
+++ try1.py
@@ -1,16 +1,37 @@
-# Authors: Artem Kii, Gaabriel Ponomarjov
+import requests
+from bs4 import BeautifulSoup
+from main import WeatherApp
 
-# Imports
-from customtkinter import *
-import time
+def get_current_temperature(city):
+    url = f"https://www.weather-forecast.com/locations/{city}/forecasts/latest"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        temperature = soup.find(class_='temp').text.strip()
+        # Преобразовываем температуру в целое число (если она представлена в виде строки)
+        temperature = str(temperature)
+        # Создаем экземпляр класса WeatherApp и передаем температуру в качестве аргумента для метода mainloop()
+        return str(temperature)
+    else:
+        print("Error: Failed to retrieve weather data.")
 
-app = CTk()
-app.title("Weather APP | Try1")
-app.geometry("1000x600")
-app.configure(bg='#1e1e1e')
-app.resizable(width=False, height=False)
+if __name__ == "__main__":
+    city = input("Enter city name: ")
+    a = get_current_temperature(city)
+#    Tallinn
+#London
 
-entry = CTkEntry(master=app, width=120, height=25, corner_radius=10, fg_color='#333333')
-entry.place(relx=0.5, rely=0.1, anchor=CENTER)
-
+app = WeatherApp()
+app.setup_ui(a)
 app.mainloop()
+
+
+
+
+
+
+
+
+
+
