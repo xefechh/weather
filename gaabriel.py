4cff6eeceeb5d7d11ef3210060da00b4d40fbf41
Gaabriel Ponomarjov <gaabriel.zachar@ivkhk.ee>
2024-05-31 10:25:25 +0300

    dfas
    fdsa

--- gaabriel.py
+++ gaabriel.py
@@ -1,10 +1,24 @@
 from customtkinter import *
+from PIL import Image
+import requests
 
+api = '58ece528195235057712455ac755125d'
 app = CTk()
 app.geometry("300x400")
 
-search = CTkEntry(app, font=("Helvetica", 18), 
-                  placeholder_text='SEARCH', width=250, height=20)
-search.pack(pady=10, anchor='w', padx=10)
+def search_click():
+  city = search.get()
+  
+
+frame = CTkFrame(app)
+frame.pack(pady=10, anchor='w', padx=10)
+
+search = CTkEntry(frame, font=("Helvetica", 18), placeholder_text='SEARCH', width=250, height=30)
+search.grid(row=0, column=0)
+
+search_icon = CTkImage(light_image=Image.open("search_icon.png"), dark_image=Image.open("search_icon.png"))
+
+search_button = CTkButton(frame, text='', image=search_icon, width=30, height=30, command=search_click, corner_radius=0)
+search_button.grid(row=0, column=1)
 
 app.mainloop()
